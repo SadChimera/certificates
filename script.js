5e12868c98365f11ed7ddc0ed62a77d75ebc4936
JenniferLee <jennifer.lee24@student.cart.org>
2025-02-07 09:55:54 -0800

    g

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function changeImage(){
-    var img = document.getElementById('centimg');
-    image.src='Mask group.png';
+let myButton = document.querySelector("button");
+let myNName = document.querySelector("p")
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `${myName}`;
 }
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myNName.textContent = `${storedName}`;
+}
+myButton.addEventListener("click", () => {
+    setUserName();
+})
+
