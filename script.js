ac62ecc27693a8bd810a36448ebd89246f8e1d37
JenniferLee <jennifer.lee24@student.cart.org>
2025-02-07 10:17:26 -0800

    df

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let myButton = document.querySelector("button");
-let myNName = document.querySelector("p")
+let myNName = document.querySelector("h3")
+let myDDate = document.querySelector("h3")
 
 function setUserName() {
-    const myName = prompt("Please enter your name.");
+    const myName = prompt("Please enter your name. (It's recommended to refresh page after name.");
     localStorage.setItem("name", myName);
-    myHeading.textContent = `${myName}`;
+    myNName.textContent = `${myName}`;
+}
+
+function setInfo() {
+    const myData = prompt("Input information.")
+    localStorage.setItem("data", myData);
+    myDDate.textContent = `${myData}`;
 }
 
 if (!localStorage.getItem("name")) {
